66985db024a53e6d6a9c7bd0202f60e8a1f287b6
1374936829@qq.com <1374936829@qq.com>
2026-03-16 11:45:46 +0800

    修复 build.gradle.kts 合并冲突

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 # Add project specific ProGuard rules here.
 -keepattributes *Annotation*
 -keepclassmembers class * {
     @android.webkit.JavascriptInterface <methods>;
 }
-=======
-# Add project specific ProGuard rules here.
--keepattributes *Annotation*
--keepclassmembers class * {
-    @android.webkit.JavascriptInterface <methods>;
-}
->>>>>>> f6e29ecc53d18f73879112d823be8df23faeea30
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 // 项目级 build.gradle
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
-=======
-// 项目级 build.gradle
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-}
->>>>>>> f6e29ecc53d18f73879112d823be8df23faeea30
